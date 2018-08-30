6c936183d2754d45a1e6c3417acbb1b3633eb763
xavier <xaviersulkowski@gmail.com>
2018-08-30 19:11:53 +0200

    Add comments' forms

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -11,4 +11,14 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
 
+
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'email', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'body', 'email')
+
+
+admin.site.register(Comment, CommentAdmin)
